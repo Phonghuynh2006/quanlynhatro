8a552f16fd9a2b5ac03ea52209dec5b23f3d5b29
phonghuynh031226 <phonghuynh031226@gmail.com>
2025-07-30 18:26:36 +0700

    phonghuynh code ui

--- src/main/java/main/ui/test.java
+++ src/main/java/main/ui/test.java
@@ -10,5 +10,18 @@ package main.ui;
  */
 public class test {
 
+    //main chính chạy dự án
+    
+        public static void main(String[] args) {
+        new xinchaoJdialog(null, true).setVisible(true);
 
+        dangnhapJdialog login = new dangnhapJdialog(null, true);
+        login.setVisible(true);
+
+        if (login.isDangNhapThanhCong()) {
+//            new quanlynhatro().setVisible(true);
+//        } else {
+            System.out.println("Đăng nhập thất bại hoặc bị hủy");
+        }
+    }
 }
