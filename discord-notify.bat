eaecf14d03e9b28bc29fd9080c73bf195fd49419
PHONG <PHONG@PHONGHUYNH>
2025-07-30 18:52:44 +0700

    phonghuynh0312226

--- discord-notify.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set WEBHOOK_URL=https://discord.com/api/webhooks/1400071331470377080/VLvk7INu2kxW_WltG5AcootWXaNplC4w9r97MzeQiDvKRIpPCuj2DkjyPZjMB6q9mXdB
-
-REM Lấy commit message của commit cuối cùng
-for /f "tokens=*" %%i in ('git log -1 --pretty=format:"%%s"') do set COMMIT_MSG=%%i
-
-REM Gửi POST request lên webhook Discord
-curl -H "Content-Type: application/json" -X POST -d "{\"content\": \"New commit: %COMMIT_MSG%\"}" %WEBHOOK_URL%
